24ce411fd827cf593e3b51d00b834d320c0d6121
Maurice Mengel <mauricemengel@gmail.com>
2021-09-27 09:42:05 +0200

    mv Replace2.py to Replace.py AGAIN

--- src/Replace.py
+++ src/Replace.py
@@ -134,9 +134,10 @@ if __name__ == "__main__":
     parser.add_argument("-l","--lazy", help="lazy modes reads search results from a file cache, for debugging", action='store_true')
     parser.add_argument("-a","--act", help="include action, without it only show what would be changed", action='store_true')
     parser.add_argument("-j","--job", help="load a plugin and use that code", required=True)
+    parser.add_argument("-L","--Limit", help="set limit for initial search", default="-1")
 
     args = parser.parse_args()
 
     replacer = Replace(baseURL=baseURL, pw=pw, user=user, lazy=args.lazy, act=args.act)
     plugin = replacer.job(plugin=args.job)
-    replacer.runPlugin(plugin=plugin, limit=1) # limit is optional
+    replacer.runPlugin(plugin=plugin, limit=args.Limit) # set to -1 for production
