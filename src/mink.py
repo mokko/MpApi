62113b7e626fb11eece7b3ac7308de6446a2405d
Maurice Mengel <mauricemengel@gmail.com>
2022-12-11 13:09:30 +0100

    mink: moving things around (refactoring)

--- src/mink.py
+++ src/mink.py
@@ -24,7 +24,7 @@ DSL COMMANDs
     getItem : save a single item to disk
     getPack : for a single id, get multi-type info (Objects, Multimedia, Persons...)
     pack    : pack together several (clean) files
-    getAttachments: downloads attachments for given module data
+    attachments: downloads attachments for given module data
 
 MPAPI CLASSES
     SEARCH -> CLIENT -> MODULE
@@ -80,127 +80,6 @@ class Mink:
         self.conf = conf
         self._parse_conf(job=job)
 
-    def _parse_conf(self, *, job: str) -> None:
-        """
-        For a job specified on command line, executes the dsl commands in that job.
-        """
-        self.job = job  # job requested to execute from command line
-        self.current_job = None  # definition in conf file
-        args: list = []
-        any_job: bool = False
-        # pretty ugly dsl parser...
-        with open(self.conf, mode="r") as file:
-            c = 0  # line counter
-            for line in file:
-                c += 1
-                uncomment = line.split("#", 1)[0].strip()
-                if uncomment.isspace() or not uncomment:
-                    continue
-                line = line.expandtabs(4)
-                indent_lvl = int((len(line) - len(line.lstrip()) + 4) / 4)
-                parts: list[str] = uncomment.split()
-                if indent_lvl == 1:  # job label
-                    if not parts[0].endswith(":"):
-                        raise SyntaxError(
-                            f"Job label has to end with colon: line {c} {parts[0]}"
-                        )
-                    self.current_job = parts[0][:-1]
-                    if self.current_job == job:
-                        right_job = True
-                        any_job = True
-                        self._mkdirs()  # also sets project_dir etc.
-                        self._init_log()
-                        self.info(f"Project dir: {self.project_dir}")
-                    else:
-                        self.project_dir: Path = Path(".")
-                        right_job = False
-                    continue
-                elif indent_lvl == 2:
-                    cmd: str = parts[0]
-                    if len(parts) > 1:
-                        args = parts[1:]
-                    else:
-                        args = []
-                    if right_job is True:
-                        # print(f"**{cmd} {args}")
-                        if cmd in allowed_commands:
-                            getattr(self, cmd)(args)
-                        else:
-                            raise SyntaxError(f"ERROR: Command {cmd} not recogized")
-                elif indent_lvl > 2:
-                    print(f"indent lvl: {indent_lvl} {parts}")
-                    raise SyntaxError("ERROR: Too many indents in dsl file")
-
-        if any_job is False:
-            print(
-                f"WARNING: User-supplied job didn't match any job from the definition file!"
-            )
-
-    #
-    # HELPERS
-    #
-
-    def _init_log(self) -> None:
-        now = datetime.datetime.now()
-        log_fn = Path(self.project_dir).joinpath(now.strftime("%Y%m%d") + ".log")
-        logging.basicConfig(
-            datefmt="%Y%m%d %I:%M:%S %p",
-            filename=log_fn,
-            filemode="a",  # append now since we're starting a new folder
-            # every day now anyways.
-            level=logging.DEBUG,
-            format="%(asctime)s: %(message)s",
-        )
-
-    def _getPart(
-        self, *, Id: int, label: str, module: str, Type: str, since: str = None
-    ) -> Module:
-        """
-        Gets a set of moduleItems depending on requested module type. Caches
-        results in a file and returns from file cache if that exists already.
-
-        Expects:
-        * Type: query type (approval, exhibit, group or loc)
-        * Id: Id of that query type
-        * module: requested target module type
-        * label: a label to be used as part of a filename (for cache)
-        * since: dateTime (optional); if provided only get items newer than
-          that date
-
-        Returns:
-        * Module objct containing the data
-        """
-        fn = self.parts_dir / f"{label}-{module}-{Type}{Id}.xml"
-        if fn.exists():
-            print(f" {module} from cache {fn}")
-            return Module(file=fn)
-        else:
-            # print (f"GH TYPE {Type}")
-            self.info(f" {module} from remote, saving to {fn}")
-            if Type == "approval":
-                m = self.sar.getByApprovalGrp(Id=Id, module=module, since=since)
-            elif Type == "exhibit":
-                # print ("***GH Exhibit")
-                m = self.sar.getByExhibit(Id=Id, module=module, since=since)
-            elif Type == "group":
-                m = self.sar.getByGroup(Id=Id, module=module, since=since)
-            elif Type == "loc":
-                m = self.sar.getByLocation(Id=Id, module=module, since=since)
-            else:
-                raise TypeError("UNKNOWN type")
-            m.toFile(path=fn)
-            return m
-
-    def _mkdirs(self) -> None:
-        date: str = datetime.datetime.today().strftime("%Y%m%d")
-        adir: Path = Path(self.job) / date
-        if not adir.is_dir():
-            Path.mkdir(adir, parents=True)
-        self.project_dir = adir
-        self.pix_dir = adir.parent / "pix"
-        # We dont need parts dir when in chunk mode
-        self.parts_dir = self.project_dir / "parts"
-
     def info(self, msg: str) -> None:
         logging.info(msg)
         print(msg)
@@ -212,6 +91,66 @@ class Mink:
     def all(self, args: list) -> None:
         self._parse_conf(job=args[0])
 
+    def attachments(self, args: list) -> None:
+        """
+        Gets (downloads) attachments for module of certain types. Usually writes
+        attachments to path as follows:
+            pix_{label}/{mulId}.{ext}
+
+        Expects:
+        * arg[0]: type (approval, exhibit, group or loc)
+        * arg[1]: id of the respective type
+        * arg[2]: label (used for filenames) or chunk
+        * arg[3]: timestamp (optional); if specified, d/l attachents to subdir pix_update
+
+        It relies on user having downloaded the respective data beforehand,i.e.
+        attachments will not download anything for you; instead it will work with
+        what you have already downloaded.
+
+        mink's dsl:
+            getPack group 123 mylabel
+            attachments group 123 mylabel
+
+            chunk group 123
+            attachments group 123 chunk
+
+        Todo: how does this work when you use updates (since parameter?)
+        """
+
+        # print(f"***{args}")
+        Type = args[0]
+        Id = args[1]
+        label = args[2]
+        try:
+            since = args[3]
+        except:
+            since = None
+
+        # determine target dir for pictures
+        if since is None:
+            pix_dir = Path(f"{self.pix_dir}_{label}")
+        else:
+            pix_dir = Path(f"{self.pix_dir}_update")
+        if not pix_dir.exists():
+            pix_dir.mkdir()
+        print(f" about to check attachments; saving to {pix_dir}")
+
+        if label == "chunk":
+            no = 1
+            mm_fn = self.project_dir
+            while mm_fn.exists():
+                mm_fn = self.project_dir / f"{Type}{Id}-chunk{n}.xml"
+                print(f" looking for multimedia info at {mm_fn}")
+                self._getAttachments(From=mm_fn)
+                no += 1
+        else:
+            # pretty dirty: assumes that getMedia has been done before
+            # todo - we could trigger a new get if this file doesn't exist
+            # or we trust the user to do the right thing?
+            mm_fn = self.parts_dir / f"{label}-Multimedia-{Type}{Id}.xml"
+            print(f" looking for Multimedia info at {mm_fn}")
+            self._getAttachments(From=mm_fn)
+
     def chunk(self, args: list) -> None:
         """
         New chunky version of getPack
@@ -220,9 +159,11 @@ class Mink:
         * args: a list with arguments that it passes along;
         * args[0]: type of the ID (approval, exhibit, group, loc, query)
         * args[1]: ID
-        * args[2]: target type (new, only optional when args[3] and args[4} are not used)
-        * args[3]: attachment (optional, only optional if args[4] is not used)
-        * args[4]: since date (optional)
+        * args[2]: target type (new, only optional when since is not used)
+        * args[3]: since date (optional)
+
+        mink's dsl
+            chunk group 123 [target] [since]
 
         NEW
         * If last run was aborted, you can restart where you left off, so exisiting chunks
@@ -236,11 +177,7 @@ class Mink:
         except:
             target: str = "Object"  # default value
         try:
-            attachment = args[3]
-        except:
-            attachment = None
-        try:
-            since: Since = args[4]
+            since: Since = args[3]
         except:
             since = None
 
@@ -267,7 +204,6 @@ class Mink:
             target=target,
             since=since,
             offset=offset,
-            attachment=attachment,
         ):
             if chunk:  # Module is true if it has more than 0 items
                 path = self.project_dir / f"{Type}{ID}-chunk{no}.xml"
@@ -276,62 +212,6 @@ class Mink:
                 chunk.toFile(path=path)
                 chunk.validate()
             no += 1
-
-    def getAttachments(self, args: list) -> None:
-        """
-        Gets (downloads) attachments for module of certain types. Usually writes
-        attachments to path as follows:
-            pix_{label}/{mulId}.{ext}
-
-        Currently, getAttachments relies on the file "{label}-Multimedia-{Type}{Id}.xml"
-        for Multimedia items. So make sure that this file exists (that getMedia has been
-        called before).
-
-        Expects:
-        * arg: list with parameters
-        * arg[0]: type (approval, exhibit, group or loc)
-        * arg[1]: id of the respective type
-        * arg[2]: label (used for filenames)
-        * arg[3]: timestamp (optional); if specified, d/l attachents to subdir pix_update
-        """
-
-        # print(f"***{args}")
-        Type = args[0]
-        Id = args[1]
-        label = args[2]
-        try:
-            since = args[3]
-        except:
-            since = None
-
-        # pretty dirty: assumes that getMedia has been done before
-        mm_fn = self.parts_dir / f"{label}-Multimedia-{Type}{Id}.xml"
-        print(f" looking for Multimedia info at {mm_fn}")
-
-        # determine target dir
-        if since is None:
-            pix_dir = Path(f"{self.pix_dir}_{label}")
-        else:
-            pix_dir = Path(f"{self.pix_dir}_update")
-        if not pix_dir.exists():
-            pix_dir.mkdir()
-        print(f" about to check attachments; saving to {pix_dir}")
-
-        mm = Module(file=mm_fn)
-        try:
-            expected = self.sar.saveAttachments(data=mm, adir=pix_dir, since=since)
-        except Exception as e:
-            self.info("Error during saveAttachments")
-            raise e
-
-            # do we want to delete those files that are no longer attached?
-            # for img in os.listdir(pix_dir):
-            #    img = pix_dir.joinpath(img)  # need resolve here
-            #    if img not in expected:
-            #        print(f"image no longer attached, removing {img}")
-            #        os.remove(img)
-        # currently we dont get attachments that have changed, but keep the same mulId,
-        # that case should be rare
 
     def getItem(self, args: list) -> Module:
         """
@@ -346,6 +226,9 @@ class Mink:
 
         returns
         * writes item to disk as side-effect
+
+        mink's dsl
+            getItem group 123
         """
         module = args[0]
         Id = args[1]
@@ -374,6 +257,9 @@ class Mink:
 
         Returns
         * None
+
+        mink's dsl
+            getPack group 123 MyLabel [since]
         """
         print(f"GET PACK {args}")
         join_fn = self.join(args)  # write join file, includes validation
@@ -433,7 +319,8 @@ class Mink:
         *-join-*.xml files in the current project directory and save to
         $label$date.xml in current working directory.
 
-        TODO: clean file has been eliminated. Change the input
+        mink's dsl
+            pack
         """
         label = str(self.project_dir.parent.name)
         date = str(self.project_dir.name)
@@ -447,3 +334,140 @@ class Mink:
                 print(f"Packing file {in_fn}")
                 m = m + Module(file=in_fn)
             m.toFile(path=pack_fn)
+
+    #
+    # HELPERS
+    #
+
+    def _init_log(self) -> None:
+        now = datetime.datetime.now()
+        log_fn = Path(self.project_dir).joinpath(now.strftime("%Y%m%d") + ".log")
+        logging.basicConfig(
+            datefmt="%Y%m%d %I:%M:%S %p",
+            filename=log_fn,
+            filemode="a",  # append now since we're starting a new folder
+            # every day now anyways.
+            level=logging.DEBUG,
+            format="%(asctime)s: %(message)s",
+        )
+
+    def _getAttachments(self, *, From: Path, since: str):
+        try:
+            expected = self.sar.saveAttachments(data=From, adir=pix_dir, since=since)
+        except Exception as e:
+            self.info("Error during saveAttachments")
+            raise e
+
+            # do we want to delete those files that are no longer attached?
+            # for img in os.listdir(pix_dir):
+            #    img = pix_dir.joinpath(img)  # need resolve here
+            #    if img not in expected:
+            #        print(f"image no longer attached, removing {img}")
+            #        os.remove(img)
+        # currently we dont get attachments that have changed, but keep the same mulId,
+        # that case should be rare
+
+    def _getPart(
+        self, *, Id: int, label: str, module: str, Type: str, since: str = None
+    ) -> Module:
+        """
+        Gets a set of moduleItems depending on requested module type. Caches
+        results in a file and returns from file cache if that exists already.
+
+        Expects:
+        * Type: query type (approval, exhibit, group or loc)
+        * Id: Id of that query type
+        * module: requested target module type
+        * label: a label to be used as part of a filename (for cache)
+        * since: dateTime (optional); if provided only get items newer than
+          that date
+
+        Returns:
+        * Module objct containing the data
+        """
+        fn = self.parts_dir / f"{label}-{module}-{Type}{Id}.xml"
+        if fn.exists():
+            print(f" {module} from cache {fn}")
+            return Module(file=fn)
+        else:
+            # print (f"GH TYPE {Type}")
+            self.info(f" {module} from remote, saving to {fn}")
+            if Type == "approval":
+                m = self.sar.getByApprovalGrp(Id=Id, module=module, since=since)
+            elif Type == "exhibit":
+                # print ("***GH Exhibit")
+                m = self.sar.getByExhibit(Id=Id, module=module, since=since)
+            elif Type == "group":
+                m = self.sar.getByGroup(Id=Id, module=module, since=since)
+            elif Type == "loc":
+                m = self.sar.getByLocation(Id=Id, module=module, since=since)
+            else:
+                raise TypeError("UNKNOWN type")
+            m.toFile(path=fn)
+            return m
+
+    def _mkdirs(self) -> None:
+        date: str = datetime.datetime.today().strftime("%Y%m%d")
+        projct_dir: Path = Path(self.job) / date
+        if not project_dir.is_dir():
+            Path.mkdir(project_dir, parents=True)
+        self.project_dir = project_dir
+        self.pix_dir = project_dir.parent / "pix"
+        # We dont need parts dir when in chunk mode, so we're making it later
+        self.parts_dir = self.project_dir / "parts"
+
+    def _parse_conf(self, *, job: str) -> None:
+        """
+        For a job specified on command line, executes the dsl commands in that job.
+        """
+        self.job = job  # job requested to execute from command line
+        self.current_job = None  # definition in conf file
+        args: list = []
+        any_job: bool = False
+        # pretty ugly dsl parser...
+        with open(self.conf, mode="r") as file:
+            c = 0  # line counter
+            for line in file:
+                c += 1
+                uncomment = line.split("#", 1)[0].strip()
+                if uncomment.isspace() or not uncomment:
+                    continue
+                line = line.expandtabs(4)
+                indent_lvl = int((len(line) - len(line.lstrip()) + 4) / 4)
+                parts: list[str] = uncomment.split()
+                if indent_lvl == 1:  # job label
+                    if not parts[0].endswith(":"):
+                        raise SyntaxError(
+                            f"Job label has to end with colon: line {c} {parts[0]}"
+                        )
+                    self.current_job = parts[0][:-1]
+                    if self.current_job == job:
+                        right_job = True
+                        any_job = True
+                        self._mkdirs()  # also sets project_dir etc.
+                        self._init_log()
+                        self.info(f"Project dir: {self.project_dir}")
+                    else:
+                        self.project_dir: Path = Path(".")
+                        right_job = False
+                    continue
+                elif indent_lvl == 2:
+                    cmd: str = parts[0]
+                    if len(parts) > 1:
+                        args = parts[1:]
+                    else:
+                        args = []
+                    if right_job is True:
+                        # print(f"**{cmd} {args}")
+                        if cmd in allowed_commands:
+                            getattr(self, cmd)(args)
+                        else:
+                            raise SyntaxError(f"ERROR: Command {cmd} not recogized")
+                elif indent_lvl > 2:
+                    print(f"indent lvl: {indent_lvl} {parts}")
+                    raise SyntaxError("ERROR: Too many indents in dsl file")
+
+        if any_job is False:
+            print(
+                f"WARNING: User-supplied job didn't match any job from the definition file!"
+            )
